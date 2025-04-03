6602e8eaf65189f120bc1741b33a36f43ca18c2a
Hafsa Hassan <xafsaxaaji936@gmail.com>
2025-04-03 21:51:51 +0300

    add php file

---
+++ library/footer.php
@@ -0,0 +1,28 @@
+<style>
+  .text-right img {
+    height: 35px;
+  }
+</style>
+
+<main class="app-content-footer" id="footer-height">
+  <div class="app-title-footer">
+    <div>
+      <p style="font: 12px 'Poppins'; font-weight: bold;">
+        <span style="font-size: 12px; font-weight: bold; text-transform: uppercase;">
+          HOSPITAL SYSTEM
+        </span><br><span style="color: #e60073; font-size: 10px; text-transform: uppercase;">
+          VERSION 1.0
+        </span>
+      </p>
+    </div>
+    <ul class="app-breadcrumb breadcrumb">
+      <li>
+        <p class="text-right">
+          <a href="#" target="_blank">
+            <img src="img/hirgal-logo.png" alt="Hospital Logo" />
+          </a>
+        </p>
+      </li>
+    </ul>
+  </div>
+</main>
