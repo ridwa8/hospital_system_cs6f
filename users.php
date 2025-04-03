303bec05e55f082e4aa1d62158b13d9c27666c9c
Hafsa Hassan <xafsaxaaji936@gmail.com>
2025-04-03 21:50:37 +0300

    add file php users

---
+++ users.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+<?php include "library/head.php";?> 
+  </head>
+  <body class="app sidebar-mini">
+    <!-- Navbar-->
+    <?php include "library/header.php";?> 
+    <!-- Sidebar menu-->
+    <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
+    <?php require "library/sidebar.php";?> 
+    <main class="app-content">
+      <div class="app-title">
+        <div>
+          <h1><i class="bi bi-speedometer"></i> Dashboard</h1>
+          <p>A free and open source Bootstrap 5 admin template</p>
+        </div>
+        <ul class="app-breadcrumb breadcrumb">
+          <li class="breadcrumb-item"><i class="bi bi-house-door fs-6"></i></li>
+          <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+        </ul>
+      </div>
+     
+
+
+    </main>
+    <?php include "library/footer.php";?> 
+    <?php include "library/script.php";?> 
+  </body>
+</html>
